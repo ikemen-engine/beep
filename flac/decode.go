10fc032bf950960128a2f708ebb3ef14aef75471
Jesuszilla <Jesuszilla@trinitymugen.net>
2024-05-11 17:52:39 -0500

    Re-implemented looping FLAC with a different lib
    go-flac library with Apache License 2.0 added to retrieve total number of samples
    goflac library with BSD 3-clause License added for decoding
    Requires libflac 1.4.2 or later (1.4.3 is best) and pkg-config tool to build.

--- flac/decode.go
+++ flac/decode.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"io"
 
+	meta "github.com/go-flac/go-flac"
+	flac "github.com/go-musicfox/goflac"
 	"github.com/ikemen-engine/beep"
-	"github.com/mewkiz/flac"
 	"github.com/pkg/errors"
 )
 
@@ -14,42 +15,50 @@ import (
 //
 // Do not close the supplied Reader, instead, use the Close method of the returned
 // StreamSeekCloser when you want to release the resources.
-func Decode(r io.Reader) (s beep.StreamSeekCloser, format beep.Format, err error) {
-	d := decoder{r: r}
+func Decode(rc io.ReadSeekCloser) (s beep.StreamSeekCloser, format beep.Format, err error) {
+	d := decoder{r: rc}
 	defer func() { // hacky way to always close r if an error occurred
 		if closer, ok := d.r.(io.Closer); ok {
 			if err != nil {
 				closer.Close()
+				err = errors.Wrap(err, "flac")
 			}
 		}
 	}()
 
-	rs, seeker := r.(io.ReadSeeker)
-	if seeker {
-		d.stream, err = flac.NewSeek(rs)
-		d.seekEnabled = true
-	} else {
-		d.stream, err = flac.New(r)
+	// First we need the metadata (just to get the # of samples)
+	var f *meta.File
+	if f, err = meta.ParseMetadata(rc); err != nil {
+		return nil, beep.Format{}, errors.Wrap(err, "flac-meta")
 	}
+	var streaminfo *meta.StreamInfoBlock
+	if streaminfo, err = f.GetStreamInfo(); err != nil {
+		return nil, beep.Format{}, errors.Wrap(err, "flac-meta")
+	}
+	d.len = streaminfo.SampleCount
 
-	if err != nil {
+	// Seek to 0 so the decoder doesn't freak out.
+	rc.Seek(0, io.SeekStart)
+	if d.stream, err = flac.NewDecoderReader(rc); err != nil {
 		return nil, beep.Format{}, errors.Wrap(err, "flac")
 	}
+
 	format = beep.Format{
-		SampleRate:  beep.SampleRate(d.stream.Info.SampleRate),
-		NumChannels: int(d.stream.Info.NChannels),
-		Precision:   int(d.stream.Info.BitsPerSample / 8),
+		SampleRate:  beep.SampleRate(d.stream.Rate),
+		NumChannels: d.stream.Channels,
+		Precision:   d.stream.Depth / 8,
 	}
+
 	return &d, format, nil
 }
 
 type decoder struct {
-	r           io.Reader
-	stream      *flac.Stream
-	buf         [][2]float64
-	pos         int
-	err         error
-	seekEnabled bool
+	r      io.Reader
+	stream *flac.Decoder
+	buf    [][2]float64
+	pos    int
+	err    error
+	len    int64
 }
 
 func (d *decoder) Stream(samples [][2]float64) (n int, ok bool) {
@@ -62,8 +71,16 @@ func (d *decoder) Stream(samples [][2]float64) (n int, ok bool) {
 		if j >= len(d.buf) {
 			// refill buffer.
 			if err := d.refill(); err != nil {
-				d.err = err
-				d.pos += n
+				// Only set the error if it's not EOF
+				if err != io.EOF {
+					d.err = err
+					d.pos += n
+				} else {
+					// Set the pos to the end if less than length because we've reached EOF (hack to work with loop)
+					if int64(d.pos) < d.len {
+						d.pos = int(d.len)
+					}
+				}
 				return n, n > 0
 			}
 			j = 0
@@ -82,56 +99,58 @@ func (d *decoder) refill() error {
 	// Empty buffer.
 	d.buf = d.buf[:0]
 	// Parse audio frame.
-	frame, err := d.stream.ParseNext()
+	frame, err := d.stream.ReadFrame()
 	if err != nil {
 		return err
 	}
+
+	// Decode audio samples.
+	bps := d.stream.Depth
+	nchannels := d.stream.Channels
 	// Expand buffer size if needed.
-	n := len(frame.Subframes[0].Samples)
+	n := len(frame.Buffer) / nchannels
 	if cap(d.buf) < n {
 		d.buf = make([][2]float64, n)
 	} else {
 		d.buf = d.buf[:n]
 	}
-	// Decode audio samples.
-	bps := d.stream.Info.BitsPerSample
-	nchannels := d.stream.Info.NChannels
 	s := 1 << (bps - 1)
 	q := 1 / float64(s)
 	switch {
 	case bps == 8 && nchannels == 1:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(int8(frame.Subframes[0].Samples[i])) * q
-			d.buf[i][1] = float64(int8(frame.Subframes[0].Samples[i])) * q
+			d.buf[i][0] = float64(int8(frame.Buffer[i])) * q
+			d.buf[i][1] = float64(int8(frame.Buffer[i])) * q
 		}
 	case bps == 16 && nchannels == 1:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(int16(frame.Subframes[0].Samples[i])) * q
-			d.buf[i][1] = float64(int16(frame.Subframes[0].Samples[i])) * q
+			d.buf[i][0] = float64(int16(frame.Buffer[i])) * q
+			d.buf[i][1] = float64(int16(frame.Buffer[i])) * q
 		}
 	case bps == 24 && nchannels == 1:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(int32(frame.Subframes[0].Samples[i])) * q
-			d.buf[i][1] = float64(int32(frame.Subframes[0].Samples[i])) * q
+			d.buf[i][0] = float64(frame.Buffer[i]) * q
+			d.buf[i][1] = float64(frame.Buffer[i]) * q
 		}
 	case bps == 8 && nchannels >= 2:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(int8(frame.Subframes[0].Samples[i])) * q
-			d.buf[i][1] = float64(int8(frame.Subframes[1].Samples[i])) * q
+			d.buf[i][0] = float64(int8(frame.Buffer[i*nchannels])) * q
+			d.buf[i][1] = float64(int8(frame.Buffer[i*nchannels+1])) * q
 		}
 	case bps == 16 && nchannels >= 2:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(int16(frame.Subframes[0].Samples[i])) * q
-			d.buf[i][1] = float64(int16(frame.Subframes[1].Samples[i])) * q
+			d.buf[i][0] = float64(int16(frame.Buffer[i*nchannels])) * q
+			d.buf[i][1] = float64(int16(frame.Buffer[i*nchannels+1])) * q
 		}
 	case bps == 24 && nchannels >= 2:
 		for i := 0; i < n; i++ {
-			d.buf[i][0] = float64(frame.Subframes[0].Samples[i]) * q
-			d.buf[i][1] = float64(frame.Subframes[1].Samples[i]) * q
+			d.buf[i][0] = float64(frame.Buffer[i*nchannels]) * q
+			d.buf[i][1] = float64(frame.Buffer[i*nchannels+1]) * q
 		}
 	default:
 		panic(fmt.Errorf("support for %d bits-per-sample and %d channels combination not yet implemented", bps, nchannels))
 	}
+	// print("GET OUTTA HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
 	return nil
 }
 
@@ -140,19 +159,15 @@ func (d *decoder) Err() error {
 }
 
 func (d *decoder) Len() int {
-	return int(d.stream.Info.NSamples)
+	return int(d.len)
 }
 
 func (d *decoder) Position() int {
-	return d.pos
+	return int(d.pos)
 }
 
 // p represents flac sample num perhaps?
 func (d *decoder) Seek(p int) error {
-	if !d.seekEnabled {
-		return errors.New("flac.decoder.Seek: not enabled")
-	}
-
 	pos, err := d.stream.Seek(uint64(p))
 	d.pos = int(pos)
 	return err
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/ikemen-engine/beep
 go 1.14
 
 require (
+	github.com/cocoonlife/testify v0.0.0-20160218172820-792cc1faeb64 // indirect
 	github.com/gdamore/tcell v1.3.0
+	github.com/go-flac/go-flac v1.0.0
+	github.com/go-musicfox/goflac v0.1.5
 	github.com/hajimehoshi/go-mp3 v0.3.0
 	github.com/hajimehoshi/oto v0.7.1
 	github.com/jfreymuth/oggvorbis v1.0.2
-	github.com/mewkiz/flac v1.0.7
 	github.com/pkg/errors v0.9.1
 	github.com/samhocevar/go-meltysynth v0.0.0-20230403180939-aca4a036cb16
 )
